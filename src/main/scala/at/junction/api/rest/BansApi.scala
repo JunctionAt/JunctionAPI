71eeffafa42a1ec9eb841247f0360b7d3abb10f1
Hansihe <hansihe@hansihe.com>
2014-04-23 17:27:53 +0200

    remove deprecated bans methods, they don't even work anymore

--- src/main/scala/at/junction/api/rest/BansApi.scala
+++ src/main/scala/at/junction/api/rest/BansApi.scala
@@ -21,21 +21,6 @@ class BansApi(api: RestApi) extends ApiModule(api) {
   case class Note(id: Integer, issuer: PlayerIdentifier, target: PlayerIdentifier, server: String,
               time: Date = null, active: Boolean, note: String)
 
-  @Deprecated
-  def getBans(username: String, active: BanStatus): util.List[Ban] = {
-    val request = GET("/anathema/bans")
-      .param("username", username)
-      .param("scope", "local")
-      .param("active", active match {
-        case BanStatus.Both => "none"
-        case BanStatus.Active => "true"
-        case BanStatus.Inactive => "false"
-      })
-
-    val json = parseApiResponse(request.asString)
-
-    (json \ "bans").extract[List[Ban]]
-  }
 
   def getBans(target: PlayerIdentifier, active: BanStatus = BanStatus.Active): util.List[Ban] = {
     val request = GET("/anathema/bans")
@@ -50,22 +35,6 @@ class BansApi(api: RestApi) extends ApiModule(api) {
     val json = parseApiResponse(request.asString)
 
     (json \ "bans").extract[List[Ban]]
-  }
-
-  @Deprecated
-  def getNotes(username: String, active: BanStatus): util.List[Note] = {
-    val request = GET("/anathema/notes")
-      .param("username", username)
-      .param("scope", "local")
-      .param("active", active match {
-        case BanStatus.Both => "none"
-        case BanStatus.Active => "true"
-        case BanStatus.Inactive => "false"
-      })
-
-    val json = parseApiResponse(request.asString)
-
-    (json \ "notes").extract[List[Note]]
   }
 
   def getNotes(target: PlayerIdentifier, active: BanStatus): util.List[Note] = {
@@ -83,32 +52,11 @@ class BansApi(api: RestApi) extends ApiModule(api) {
     (json \ "notes").extract[List[Note]]
   }
 
-  @Deprecated
-  def addNote(username: String, issuer: String, message: String) = {
-    val request = POST("/anathema/notes", asUser = issuer)
-      .param("server", api.getServer)
-      .param("username", username)
-      .param("note", message)
-
-    val json = parseApiResponse(request.asString)
-  }
-
-
   def addNote(target: PlayerIdentifier, issuer: PlayerIdentifier, message: String) = {
     val request = POST("/anathema/notes", asPlayer = issuer)
       .param("server", api.getServer)
       .param("uuid", target.mojangUUID)
       .param("note", message)
-
-    val json = parseApiResponse(request.asString)
-  }
-
-  @Deprecated
-  def addBan(username: String, issuer: String, reason: String) = {
-    val request = POST("/anathema/bans", asUser = issuer)
-      .param("server", api.getServer)
-      .param("username", username)
-      .param("reason", reason)
 
     val json = parseApiResponse(request.asString)
   }
@@ -125,14 +73,6 @@ class BansApi(api: RestApi) extends ApiModule(api) {
   def delNote(id: Integer, issuer: PlayerIdentifier) = {
     val request = DELETE("/anathema/notes", asPlayer = issuer)
       .param("id", id.toString)
-
-    val json = parseApiResponse(request.asString)
-  }
-
-  @Deprecated
-  def delBan(username: String, issuer: String) = {
-    val request = DELETE("/anathema/bans", asUser = issuer)
-      .param("username", username)
 
     val json = parseApiResponse(request.asString)
   }
